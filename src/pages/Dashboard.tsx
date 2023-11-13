266fa74fb8ae58b675b7e0871a4e891dde943e27
Thomas <thomasjji14@gmail.com>
2023-11-13 08:59:53 -0500

    add basic prescrption box to dashboard(WIP)

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
+import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
 import Divider from '@mui/material/Divider';
 import Drawer from '@mui/material/Drawer';
+import Icon from '@mui/material/Icon';
 import IconButton from '@mui/material/IconButton';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import List from '@mui/material/List';
@@ -11,10 +13,12 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
+import MedicationIcon from '@mui/icons-material/Medication';
 import MailIcon from '@mui/icons-material/Mail';
 import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
 import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
 import { Link } from 'react-router-dom';
 import { MainLayout } from '../components/MainLayout';
@@ -45,7 +49,41 @@ export function DashboardPage(props: Props) {
 
 
 
-    return <></>;
+    return <Box sx={{ margin:'20px', display:'flex', width:'100%'}}>
+        <Box sx={{ backgroundColor: 'white', borderRadius: '10px', padding: '20px', border: '1px solid lightgrey' }}>
+            <Box display='flex' alignItems='center'>
+                <Stack direction="row" spacing={20}>
+                    <Typography variant='h4' align='left'>
+                        Prescriptions
+                    </Typography>
+                    <MedicationIcon />
+                </Stack>
+            </Box>
+            <Divider style={{ backgroundColor: 'grey' }} />
+            <Typography variant="body1" align="left">
+            <List>
+                <ListItem>
+                    <ListItemIcon>
+                        <FiberManualRecordIcon sx={{ fontSize: 'small' }} />
+                    </ListItemIcon>
+                    <ListItemText primary='Antidepressants' secondary='Prescribed on 01/20/2022'/>
+                </ListItem>
+                <ListItem>
+                    <ListItemIcon>
+                        <FiberManualRecordIcon sx={{ fontSize: 'small' }} />
+                    </ListItemIcon>
+                    <ListItemText primary='Fever Reducer' secondary='Prescribed on 01/20/2022'/>
+                </ListItem>
+                <ListItem>
+                    <ListItemIcon>
+                        <FiberManualRecordIcon sx={{ fontSize: 'small' }} />
+                    </ListItemIcon>
+                    <ListItemText primary='Topical Ointment' secondary='Prescribed on 01/20/2022'/>
+                </ListItem>
+                </List>
+            </Typography>
+        </Box>
+    </Box>;
 
 }
 
