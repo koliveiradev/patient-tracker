aec2f0923464cd8ad2777440f646d339de51e7c1
Brandon  Saulnier <124940268+b-saulnier@users.noreply.github.com>
2023-12-04 15:13:17 -0500

    added additional login service to Patient

--- src/services/Patient.tsx
+++ src/services/Patient.tsx
@@ -19,10 +19,48 @@ export class DatabaseService {
         return data;
     }
 
+    async getPatientID(email: string): Promise<any> {
+        const { data, error } = await supabase.from('patients').select('*').filter('email', 'eq', email).limit(1).single();
+        return data.id;
+    }
+
     async getPatientData(id: string): Promise<any> {
-        const { data, error } = await supabase.from('patients').select('*, events ( *, diagnoses ( *, illness:illness_id ( * ), prescriptions (*,medication:medication_id(*)) ) )').filter('id', 'eq', id).limit(1).single();
+        const { data, error } = await supabase.from('event_doctor_view').select('*, events ( *, diagnoses ( *, illness:illness_id ( * ), prescriptions (*,medication:medication_id(*)) ) )').filter('patient_id', 'eq', id);
         console.log(data);
         return data;
+    }
+
+    async getLoggedInPatient(): Promise<any> {
+        console.log("finding logged in patient...")
+        const {
+            data: { user },
+        } = await supabase.auth.getUser();
+
+        console.log(user);
+        console.log(user?.email!);
+
+        var patient_id = await this.getPatientID(user?.email!);
+        console.log(patient_id)
+        return patient_id;
+    }
+
+    async getUpcomingEvents(id: string): Promise<any> {
+        const { data, error } = await supabase.from('event_doctor_view')
+            .select('*')
+            .filter('patient_id', 'eq', id)
+            // .filter('start_date', 'gte', new Date())
+            // .order('start_date', { ascending: true })
+            // .limit(10);
+        
+        // filter on Date
+        const today = new Date();
+        const filteredData = (data == null)? null : data.filter((e: any) => {
+            console.log(e);
+            return new Date(e.end_time) >= today;
+        });
+
+        console.log("getUpcomingEvents", filteredData);
+        return filteredData;
     }
 
     async getVisit(id: string): Promise<any> {
