eb999fdb935373d8b8f893bae2ba0be2c956ccf4
Brandon  Saulnier <124940268+b-saulnier@users.noreply.github.com>
2023-12-13 16:28:45 -0500

    made patient service faster to lighthouse 82

--- src/models/Event.ts
+++ src/models/Event.ts
@@ -1,8 +1,8 @@
 export interface Event { // View of events joined with associated Doctor
     id: number;
     patient_id: number;
-    start_time: string;
-    end_time: string;
+    start_time: string; // timestamptz
+    end_time: string; // timestamptz
     type: string;
     doctor_id: number;
     first_name: string; // doctor first name, last name
